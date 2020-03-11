473c24e39a907124e4a1a8429f2ce6c1f38c034e
TuTejsy <1111kaktus1111@gmail.com>
2020-03-11 23:16:31 +0300

    add solution to task1

--- RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
+++ RSSchool_T1/RSSchool_T1/Exercises/1/T1Array.m
@@ -4,7 +4,27 @@
 
 // Complete the following fuction
 - (NSArray *)convertToHappy:(NSArray *)sadArray {
-    return @[];
+    if (sadArray.count == 0) {
+        return sadArray;
+    }
+    
+    NSMutableArray* resultArray = [[NSMutableArray alloc] initWithArray:sadArray];
+    
+    for (int i = 1; i < resultArray.count - 1 ; i++) {
+        int value = [resultArray[i] intValue];
+        int sum = [resultArray[i-1] intValue] + [resultArray[i+1] intValue];
+        
+        if (value > sum) {
+            [resultArray removeObjectAtIndex: i];
+            i--;
+            
+            if (i > 1) {
+                i--;
+            }
+        }
+    }
+    
+    return (NSArray*)resultArray;
 }
 
 @end
