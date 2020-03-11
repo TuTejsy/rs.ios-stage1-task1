1819883c36889068622e59151c6be71733a96138
TuTejsy <1111kaktus1111@gmail.com>
2020-03-11 23:41:04 +0300

    add solution to task2

--- RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/2/MiniMaxArrayConverter.m
@@ -4,7 +4,29 @@
 
 // Complete the convertFromArray function below.
 - (NSArray<NSNumber*>*)convertFromArray:(NSArray<NSNumber*>*)array {
-    return @[@0];
+    int min;
+    int max = 0;
+    int sum = 0;
+    
+    for (NSNumber* num in array) {
+        sum += [num intValue];
+    }
+    
+    min = sum;
+    
+    for (NSNumber* num in array) {
+        int sumWithoutNum = sum - [num intValue];
+        
+        if (sumWithoutNum > max) {
+            max = sumWithoutNum;
+        }
+        
+        if (sumWithoutNum < min) {
+            min = sumWithoutNum;
+        }
+    }
+    
+    return @[@(min), @(max)];
 }
 
 @end
