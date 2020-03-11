bddfa4c15ecff2b899b8d73ebeae5127e747129d
TuTejsy <1111kaktus1111@gmail.com>
2020-03-12 00:07:03 +0300

    add solution to task3

--- RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
+++ RSSchool_T1/RSSchool_T1/Exercises/3/BillCounter.m
@@ -4,7 +4,20 @@
 
 // Complete the following fuction
 - (NSString*)compareResultForBill:(NSArray<NSNumber*>*)bill notIncludingElementWithIndex:(NSInteger)index withGivenSum:(NSNumber*)sum {
-    return @"";
+    int currentSum = 0;
+    
+    NSMutableArray* AnnaOrder = [NSMutableArray arrayWithArray:bill];
+    [AnnaOrder removeObjectAtIndex:index];
+    
+    for (NSNumber* price in AnnaOrder) {
+        currentSum += [price intValue];
+    }
+    
+    if ([sum isEqualToNumber:@(currentSum / 2)]) {
+        return @"Bon Appetit";
+    } else {
+        return [NSString stringWithFormat: @"%i", ([sum intValue] - currentSum / 2)];
+    }
 }
 
 @end
