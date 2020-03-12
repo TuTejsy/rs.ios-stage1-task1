1ea6e3d76d57a1d59fed7bdb9c067351441b703e
TuTejsy <1111kaktus1111@gmail.com>
2020-03-12 21:12:54 +0300

    add solution to task 5

--- RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
+++ RSSchool_T1/RSSchool_T1/Exercises/4/Parser.m
@@ -4,7 +4,90 @@
 
 // Complete the parseString function below.
 - (NSArray <NSString*>*)parseString:(NSString*)string {
-    return @[@"Test"];
+    NSArray *result = @[];
+    
+    NSMutableArray *openBracketsStack = [[NSMutableArray alloc] init];
+    NSMutableArray *openBracketsObjStack = [[NSMutableArray alloc] init];
+    NSMutableArray *openBracketsPositionsStack = [[NSMutableArray alloc] init];
+    
+    NSMutableArray *closeBracketsQueue = [[NSMutableArray alloc] init];
+    NSMutableArray *closeBracketsObjQueue = [[NSMutableArray alloc] init];
+    NSMutableArray *closeBracketsPositionsQueue = [[NSMutableArray alloc] init];
+    
+    NSDictionary *openBracketsPairs = @{
+        @"(": @")",
+        @"[": @"]",
+        @"<": @">",
+    };
+    
+    NSDictionary *closeBracketsPairs = @{
+        @")": @"(",
+        @"]": @"[",
+        @">": @"<",
+    };
+
+    
+    for (int i = 0; i < [string length]; i++) {
+        NSString *ch = [string substringWithRange:NSMakeRange(i, 1)];
+        
+        if ([openBracketsPairs objectForKey:ch]) {
+            [openBracketsStack addObject:ch];
+            [openBracketsPositionsStack addObject:@(i)];
+            [openBracketsObjStack addObject:@{
+                @"bracket": ch,
+                @"position": @(i),
+            }];
+        } else if ([closeBracketsPairs objectForKey:ch]) {
+            [closeBracketsQueue addObject:ch];
+            [closeBracketsPositionsQueue addObject:@(i)];
+            [closeBracketsObjQueue addObject:@{
+                @"bracket": ch,
+                @"position": @(i),
+            }];
+        }
+    }
+    
+    for (NSDictionary *openBracketObj in openBracketsObjStack) {
+        NSString *openBracket = [openBracketObj objectForKey:@"bracket"];
+        int openBracketPosition = [[openBracketObj objectForKey:@"position"] intValue] + 1;
+
+        for (NSDictionary *closeBracketObj in closeBracketsObjQueue) {
+            if ([[closeBracketObj objectForKey:@"bracket"] isEqualToString: [openBracketsPairs objectForKey: openBracket]]) {
+                int closeBracketPosition = [[closeBracketObj objectForKey:@"position"] intValue];
+
+                result = [result arrayByAddingObject: [string substringWithRange:NSMakeRange(openBracketPosition, closeBracketPosition - openBracketPosition)]];
+
+                [closeBracketsObjQueue removeObject:closeBracketObj];
+                break;
+            }
+        }
+    }
+    
+    closeBracketsQueue = [[[closeBracketsQueue reverseObjectEnumerator] allObjects] mutableCopy];
+    closeBracketsPositionsQueue = [[[closeBracketsPositionsQueue reverseObjectEnumerator] allObjects] mutableCopy];
+    
+//    while(openBracketsStack.count != 0) {
+//        NSString *openBracket = openBracketsStack.lastObject;
+//        int openBracketPosition = [openBracketsPositionsStack.lastObject intValue] + 1;
+//        
+//        int closeBracketIndex = [closeBracketsQueue indexOfObject:[openBracketsPairs objectForKey:openBracket]];
+//        int closeBracketPosition = [closeBracketsPositionsQueue[closeBracketIndex] intValue];
+//        
+//        
+//        if (closeBracketPosition > openBracketPosition) {
+//            result = [result arrayByAddingObject: [string substringWithRange:NSMakeRange(openBracketPosition, closeBracketPosition - openBracketPosition)]];
+//            
+//            [closeBracketsQueue removeObjectAtIndex:closeBracketIndex];
+//            [closeBracketsPositionsQueue removeObjectAtIndex:closeBracketIndex];
+//        }
+//        
+//        [openBracketsStack removeLastObject];
+//        [openBracketsPositionsStack removeLastObject];
+//    }
+//    
+//    result = [[result reverseObjectEnumerator] allObjects];
+    
+    return result;
 }
 
 @end
--- RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
+++ RSSchool_T1/RSSchool_T1/Exercises/5/PalindromeSolver.m
@@ -4,7 +4,41 @@
 
 // Complete the highestValuePalindrome function below.
 - (NSString *) highestValuePalindrome:(NSString *)s n:(NSNumber *)n k:(NSNumber *)k {
-    return @"";
+    NSMutableString *resultString = [s mutableCopy];
+    int intK = [k intValue];
+    
+    for (int i = 0; i < [n intValue] / 2; i++) {
+        int rightPos = [n intValue] - i - 1;
+        
+        NSString *leftNum = [s substringWithRange:NSMakeRange(i, 1)];
+        NSString *rightNum = [s substringWithRange:NSMakeRange(rightPos, 1)];
+        
+        if (![leftNum isEqualToString:rightNum]) {
+            if (intK == 0) {
+                return @"-1";
+            }
+            
+            if ([leftNum compare:rightNum] == 1) {
+                [resultString replaceCharactersInRange:NSMakeRange(rightPos, 1) withString:leftNum];
+                intK--;
+            } else {
+                [resultString replaceCharactersInRange:NSMakeRange(i, 1) withString:rightNum];
+                intK--;
+            }
+        }
+    }
+    
+    for (int i = 0; i < [n intValue] / 2 && intK > 0; i++) {
+        int rightPos = [n intValue] - i - 1;
+        
+        [resultString replaceCharactersInRange:NSMakeRange(0, 1) withString:@"9"];
+        [resultString replaceCharactersInRange:NSMakeRange(rightPos, 1) withString:@"9"];
+        
+        
+        intK--;
+    }
+    
+    return [resultString copy];
 }
 
 @end
